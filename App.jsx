615e01c0516c9df5e2124c8f8bb7459cd6ad7aea
Devansh Chandra <155964011+Devansh149@users.noreply.github.com>
2025-12-14 16:52:52 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import data from "./data";
+import { useState } from "react";
+const App = () => {
+  const [activeId, setactiveId] = useState(null);
+  const handleClick = (id) => {
+    (activeId === id) ? setactiveId(null) : setactiveId(id);
+  }
+  return <div className="bg-gray-900 text-white h-screen w-full p-3 flex flex-col gap-7 items-center justify-center">
+    <h1 className="text-5xl font-bold">Accordian</h1>
+    <div className="flex gap-5 flex-col items-center">
+      {data.map(({ id, heading, description }) => {
+        return <div key={id}
+          onClick={() => { handleClick(id) }}
+          className="bg-amber-50 text-black border w-3xl p-3 rounded">
+          <h2>{heading}</h2>
+          {activeId==id && <p>{description}</p> }
+        </div>
+      })}
+    </div>
+  </div>
+};
+
+export default App
---
+++ data.js
@@ -0,0 +1,29 @@
+const data=[
+  {
+    "id": 1,
+    "heading": "What is React?",
+    "description": "React is a powerful JavaScript library developed by Meta for building fast, interactive, and component-based user interfaces. Its virtual DOM system makes updates efficient, and its modular structure helps developers create scalable applications."
+  },
+  {
+    "id": 2,
+    "heading": "How does an Accordion work?",
+    "description": "An accordion is a UI pattern used to show and hide content sections. When the user clicks a header, the corresponding content expands while keeping the rest collapsed, which helps maintain a clean and organized interface, especially when dealing with large amounts of text."
+  },
+  {
+    "id": 3,
+    "heading": "Why use Components in React?",
+    "description": "Components allow developers to break the UI into independent, reusable pieces that can be managed separately. This helps maintain clean code, improves debugging, and enables rapid development by reusing logic and UI patterns across an application."
+  },
+  {
+    "id": 4,
+    "heading": "What is State Management?",
+    "description": "State management refers to how data is stored, updated, and shared within an application. In React, state determines how components behave and render. Tools like useState, useReducer, and external libraries like Redux help keep data predictable and manageable."
+  },
+  {
+    "id": 5,
+    "heading": "What are React Hooks?",
+    "description": "React Hooks allow you to use features like state and lifecycle methods inside functional components. Common hooks include useState for managing state, useEffect for handling side effects, and useRef for accessing DOM elements or storing values across renders."
+  }
+]
+
+export default data
