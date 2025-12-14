b2baef061e96a9885c5c65665c54245e45b3955a
Devansh Chandra <155964011+Devansh149@users.noreply.github.com>
2025-12-14 17:10:36 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -6,13 +6,13 @@ const App = () => {
   const handleClick = (id) => {
     (activeId === id) ? setactiveId(null) : setactiveId(id);
   }
-  return <div className="bg-gray-900 text-white h-screen w-full p-3 flex flex-col gap-7 items-center justify-center">
+  return <div className="bg-black text-white h-screen w-full p-3 flex flex-col gap-7 items-center justify-center">
     <h1 className="text-5xl font-bold">Accordian</h1>
     <div className="flex gap-5 flex-col items-center">
       {data.map(({ id, heading, description }) => {
         return <div key={id}
           onClick={() => { handleClick(id) }}
-          className="bg-amber-50 text-black border w-3xl p-3 rounded">
+          className="bg-gray-900 text-white border w-3xl p-3 rounded">
           <div className="flex justify-between">
             <h2>{heading}</h2>
             {activeId == id ? <ArrowUp /> : <ArrowDown />}
